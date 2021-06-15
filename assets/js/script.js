44776c81ea8ac701266151f0468e2961e53b9ad8
hotbagels <81516937+hotbagels@users.noreply.github.com>
2021-06-14 23:44:41 -0400

    successfully have a report for current weather, and next 5 day forecast. dynamic icons working. things to still do.. color reactive uv index infor for current weather display, and local storage of past visitied city data. HTML and JS both have very DRY design to them. could use a second look once all other goals are met

--- assets/js/script.js
+++ assets/js/script.js
@@ -41,8 +41,9 @@ function handleSearchFormSubmit(event) {
             var iconURL = "http://openweathermap.org/img/wn/" + iconCode + ".png";
 
 
-           
+            
             cityResultsMainEl.textContent = data.name + " (" + now + ") ";
+            $("#result-icon").html("<img src='http://openweathermap.org/img/w/" + data.weather[0].icon + ".png' alt='Icon depicting current weather.'>");
             dailyTemp.textContent = "Temp: " + data.main.temp + "°F";
             dailyWind.textContent = "Wind: " + data.wind.speed + " MPH"
             dailyHumidity.textContent = "Humidity: " + data.main.humidity + " %";
@@ -51,14 +52,45 @@ function handleSearchFormSubmit(event) {
             console.log(data)
         })
     
-        
+
         fetch(fiveDayWeatherQueryURL)
         .then(function (response) {
             return response.json();
         })
         .then(function (data) {
             console.log(data)
-            var fiveDayTemp = document.querySelector('#5day-temp');
+
+
+            document.querySelector('#fiveDayDate-01').textContent = moment(data.list[3].dt_txt).format("MM/DD/YYYY");
+            $('#fiveDayIcon-01').html("<img src='http://openweathermap.org/img/w/" + data.list[3].weather[0].icon + ".png' alt='Icon depicting current weather.'>");
+            document.querySelector('#fiveDayTemp-01').textContent = "Temp: " + data.list[3].main.temp + "°F";
+            document.querySelector('#fiveDayWind-01').textContent = "Wind: " + data.list[3].wind.speed + " MPH";
+            document.querySelector('#fiveDayHum-01').textContent = "Humidity " + data.list[3].main.temp + " %";
+
+            document.querySelector('#fiveDayDate-02').textContent = moment(data.list[11].dt_txt).format("MM/DD/YYYY");
+            $('#fiveDayIcon-02').html("<img src='http://openweathermap.org/img/w/" + data.list[11].weather[0].icon + ".png' alt='Icon depicting current weather.'>");
+            document.querySelector('#fiveDayTemp-02').textContent = "Temp: " + data.list[11].main.temp + "°F";
+            document.querySelector('#fiveDayWind-02').textContent = "Wind: " + data.list[11].wind.speed + " MPH";
+            document.querySelector('#fiveDayHum-02').textContent = "Humidity " + data.list[11].main.temp + " %";
+
+            document.querySelector('#fiveDayDate-03').textContent = moment(data.list[19].dt_txt).format("MM/DD/YYYY");
+            $('#fiveDayIcon-03').html("<img src='http://openweathermap.org/img/w/" + data.list[19].weather[0].icon + ".png' alt='Icon depicting current weather.'>");
+            document.querySelector('#fiveDayTemp-03').textContent = "Temp: " + data.list[19].main.temp + "°F";
+            document.querySelector('#fiveDayWind-03').textContent = "Wind: " + data.list[19].wind.speed + " MPH";
+            document.querySelector('#fiveDayHum-03').textContent = "Humidity " + data.list[19].main.temp + " %";
+
+            document.querySelector('#fiveDayDate-04').textContent = moment(data.list[27].dt_txt).format("MM/DD/YYYY");
+            $('#fiveDayIcon-04').html("<img src='http://openweathermap.org/img/w/" + data.list[27].weather[0].icon + ".png' alt='Icon depicting current weather.'>");
+            document.querySelector('#fiveDayTemp-04').textContent = "Temp: " + data.list[27].main.temp + "°F";
+            document.querySelector('#fiveDayWind-04').textContent = "Wind: " + data.list[27].wind.speed + " MPH";
+            document.querySelector('#fiveDayHum-04').textContent = "Humidity " + data.list[27].main.temp + " %";
+
+            document.querySelector('#fiveDayDate-05').textContent = moment(data.list[35].dt_txt).format("MM/DD/YYYY");
+            $('#fiveDayIcon-05').html("<img src='http://openweathermap.org/img/w/" + data.list[35].weather[0].icon + ".png' alt='Icon depicting current weather.'>");
+            document.querySelector('#fiveDayTemp-05').textContent = "Temp: " + data.list[35].main.temp + "°F";
+            document.querySelector('#fiveDayWind-05').textContent = "Wind: " + data.list[35].wind.speed + " MPH";
+            document.querySelector('#fiveDayHum-05').textContent = "Humidity " + data.list[35].main.temp + " %";
+
 
         })
 }
